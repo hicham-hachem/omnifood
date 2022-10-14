d9011d521c2deab25b8821ffa704bad572c36248
hicham <hicham.hachem1@gmail.com>
2022-10-14 21:52:56 +0300

    Adding favicon and meta description

--- js/script.js
+++ js/script.js
@@ -68,3 +68,26 @@ const obs = new IntersectionObserver(
     }
 );
 obs.observe(sectionHeroEl);
+
+////////////////////////////////////////////////////////////
+
+// Fixing gap property missing in some safari version
+function checkFlexGap() {
+    var flex = document.createElement("div");
+    flex.style.display = "flex";
+    flex.style.flexDirection = "column";
+    flex.style.rowGap = "1px";
+
+    flex.appendChild(document.createElement("div"));
+    flex.appendChild(document.createElement("div"));
+
+    document.body.appendChild(flex);
+    var isSupported = flex.scrollHeight === 1;
+    flex.parentNode.removeChild(flex);
+    console.log(isSupported);
+
+    if (!isSupported)
+        document.body.classList.add("no-flexbox-gap");
+}
+
+checkFlexGap();
